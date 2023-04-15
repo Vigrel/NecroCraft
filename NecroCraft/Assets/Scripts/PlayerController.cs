56d860e4d92c10885ba8f6f9773bd2cb697aaf04
franbvc <franbvc@gmail.com>
2023-04-15 11:27:12 -0300

    (fix): health bar position

--- NecroCraft/Assets/Scripts/PlayerController.cs
+++ NecroCraft/Assets/Scripts/PlayerController.cs
@@ -6,58 +6,73 @@ using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
-	[SerializeField] private GameObject villagersParent;
+    [SerializeField] private GameObject villagersParent;
 
     public float speed = 2;
     public float maxHp;
-	public RectTransform barImage;
-	public float damageTimer = 0.5f;
+    public RectTransform barImage;
+    public float damageTimer = 0.5f;
 
     private float _currentHp;
-	private float _movementX;
-	private float _movementY;
-	private float _HPbarMaxSize;
-	private float _lastDamageTime;
+    private float _movementX;
+    private float _movementY;
+    private float _HPbarMaxSize;
+    private float _lastDamageTime;
+    private Vector2 _startingHealthBarPosition;
 
 
     public void SetHealth()
     {
-		float newSize = _HPbarMaxSize*(_currentHp/maxHp);
-		//barImage.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, newSize);
-		barImage.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Left, 363.5f, newSize);
+        float newSize = _HPbarMaxSize * (_currentHp / maxHp);
+
+        //barImage.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, newSize);
+        //barImage.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Left, 363.5f, newSize);
+
+        barImage.sizeDelta = new Vector2(newSize, barImage.sizeDelta.y);
+        barImage.anchoredPosition = new Vector2(_startingHealthBarPosition.x - (_HPbarMaxSize - newSize)/2,
+            _startingHealthBarPosition.y);
+        
+        
+        Debug.Log(newSize);
+        Debug.Log(_HPbarMaxSize);
     }
 
-	void Start ()
-	{
-		_HPbarMaxSize = barImage.rect.width;
-		_currentHp = maxHp;
-	}
+    void Start()
+    {
+        _HPbarMaxSize = barImage.rect.width;
+        _currentHp = maxHp;
+        _startingHealthBarPosition = barImage.anchoredPosition;
+    }
 
     // Start is called before the first frame update
-	void FixedUpdate ()
-	{
-		Vector2 movement = new Vector2 (_movementX, _movementY);
-		transform.Translate(movement * (speed * Time.deltaTime));
-		
-	}
-	void OnMove(InputValue value)
-	{
-		Vector2 v = value.Get<Vector2>();
+    void FixedUpdate()
+    {
+        Vector2 movement = new Vector2(_movementX, _movementY);
+        transform.Translate(movement * (speed * Time.deltaTime));
+    }
 
-		_movementX = v.x;
-		_movementY = v.y;
-	}
-    void OnCollisionStay2D(Collision2D other) 
-	{
-		float damageElapsedTime = Time.fixedTime - _lastDamageTime;
-		if (damageElapsedTime < damageTimer) return;
-		
-		if(other.gameObject.CompareTag("MeleeEnemy")){
-			_currentHp -= 1.0f;
-			SetHealth();
-		}
-		if(_currentHp==0){
-			Time.timeScale = 0;
-		}
-	}
+    void OnMove(InputValue value)
+    {
+        Vector2 v = value.Get<Vector2>();
+
+        _movementX = v.x;
+        _movementY = v.y;
+    }
+
+    void OnCollisionStay2D(Collision2D other)
+    {
+        float damageElapsedTime = Time.fixedTime - _lastDamageTime;
+        if (damageElapsedTime < damageTimer) return;
+
+        if (other.gameObject.CompareTag("MeleeEnemy"))
+        {
+            _currentHp -= 1.0f;
+            SetHealth();
+        }
+
+        if (_currentHp == 0)
+        {
+            Time.timeScale = 0;
+        }
+    }
 }
