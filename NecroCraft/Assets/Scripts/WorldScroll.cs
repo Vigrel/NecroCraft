2d45ede92e60c43d518bd5f5814fe7695172d008
Vigrel <eller.vini@gmail.com>
2023-04-20 15:57:24 -0300

    (fix): refactor generation for -1 -> 1 player positions

--- NecroCraft/Assets/Scripts/WorldScroll.cs
+++ NecroCraft/Assets/Scripts/WorldScroll.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -22,38 +23,40 @@ public class WorldScroll : MonoBehaviour
     private void Update()
     {
         _playerTilePosition = playerTransform.position / tileSize;
-        _playerTileGridPosition = CalculatePositionOnAxis(playerTransform.position);
+        _playerTileGridPosition = CalculatePositionOnAxis(_playerTilePosition);
+        Debug.Log(_playerTileGridPosition);
+        Debug.Log(_playerTilePosition);
         UpdateTilesOnScreen();
     }
 
     private void UpdateTilesOnScreen()
     {
         Vector2 pos = _playerTileGridPosition;
+
         for (int povx = -(terrainTileHorizontalCount / 2); povx <= (terrainTileHorizontalCount / 2); povx++)
         {
             for (int povy = -(terrainTileVerticalCount / 2); povy <= (terrainTileVerticalCount / 2); povy++)
-            {
+            {   
+
                 pos.x = (_playerTileGridPosition.x + povx + terrainTileHorizontalCount) % terrainTileHorizontalCount;
                 pos.y = (_playerTileGridPosition.y + povy + terrainTileVerticalCount) % terrainTileVerticalCount;
                 GameObject tile = _terrainTiles[(int)pos.x, (int)pos.y];
-                tile.transform.position = CalculateTilePosition(
-                    (int)(_playerTilePosition.x + povx),
-                    (int)(_playerTilePosition.y + povy)
+                tile.transform.position = new Vector3(
+                    (int)((int)_playerTilePosition.x + povx) * tileSize,
+                    (int)((int)_playerTilePosition.y + povy) * tileSize,
+                    1f
                 );
             }
         }
     }
 
-    private Vector3 CalculateTilePosition(int v1, int v2)
-    {
-        return new Vector3(v1 * tileSize, v2 * tileSize, 1f);
-    }
-
     private Vector2 CalculatePositionOnAxis(Vector2 currentValue)
-    {
-        currentValue = new Vector2(Mathf.FloorToInt(currentValue.x / tileSize), Mathf.FloorToInt(currentValue.y / tileSize));
-        currentValue.x = (currentValue.x + terrainTileHorizontalCount) % terrainTileHorizontalCount;
-        currentValue.y = (currentValue.y + terrainTileVerticalCount) % terrainTileVerticalCount;
+    {  
+        currentValue.x = 1+((int)currentValue.x + terrainTileHorizontalCount) % terrainTileHorizontalCount;
+        currentValue.y = 1+((int)currentValue.y + terrainTileVerticalCount) % terrainTileVerticalCount;
+
+        if (currentValue.x == 3) currentValue.x = 0;
+        if (currentValue.y == 3) currentValue.y = 0;
         return currentValue;
     }
 
@@ -61,4 +64,6 @@ public class WorldScroll : MonoBehaviour
     {
         _terrainTiles[(int)tilePosition.x, (int)tilePosition.y] = tileGameObject;
     }
+    
 }
+
